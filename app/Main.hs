001e883b85d7250b15f8056556141942c2108987
Chris Pollard <cspollard@gmail.com>
2016-08-18 15:05:11 +0100

    sort of working.

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,33 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Main where
+
+import Debug.Trace
 
 import Foreign.C.Types (CInt)
 
 import Data.Word (Word32)
+
+import Control.Concurrent.STM
+import Data.Conduit.TQueue
+import Data.Conduit.TMChan
 
 import Control.Monad (unless)
 import Control.Monad.IO.Class
 
 import Control.Lens
 
-import SDL hiding (Event)
-import qualified SDL.Event as SE
+import SDL
 
 import Linear.Affine
 import Linear.V2
 
-import Control.FRPNow
+import Conduit
 
-type SEvent = SE.Event
 makeLenses ''EventPayload
 
 textureSize :: MonadIO m => Texture -> m (V2 CInt)
@@ -30,20 +37,20 @@ textureSize t = do ti <- queryTexture t
                    return $ V2 w h
 
 
-data Redraw = Redraw
-type MyEvent = Either Redraw SEvent
+data Redraw = Redraw deriving Show
+type MyEvent = Either Redraw Event
 
 
-setFPS :: Word32 -> Now (EvStream Redraw)
-setFPS t = do (es, cb) <- callbackStream
-              addTimer t $ \_ -> do cb Redraw; return (Reschedule t)
-              return es
+timer :: MonadIO m => Word32 -> Source m Redraw
+timer fps = do q <- liftIO . atomically $ newTBQueue 1
+               addTimer fps $ \_ -> do atomically $ writeTBQueue q Redraw
+                                       return (Reschedule fps)
+               sourceTBQueue q
 
 
-sevents :: Now (EvStream SEvent)
-sevents = do (es, cb) <- callbackStream
-             sync $ mapEvents cb
-             return es
+events :: MonadIO m => Source m Event
+events = do yield =<< waitEvent
+            events
 
 
 main :: IO ()
@@ -53,17 +60,24 @@ main = do initializeAll
           bkg <- createTextureFromSurface renderer =<< loadBMP "data/bkg.bmp"
           face <- createTextureFromSurface renderer =<< loadBMP "data/face.bmp"
           ts <- textureSize bkg
-          windowSize window $= ts
-          runNowMaster $ do tstream <- fmap Left <$> setFPS 30
-                            sevts <- fmap Right <$> sevents
-                            callIOStream (playFace bkg face renderer) (merge tstream sevts)
-                            async $ return ()
+          windowSize window SDL.$= ts
+
+          evts <- runResourceT $ mapOutput Right events >=< mapOutput Left (timer 500)
+          evts $$ playFace bkg face renderer
 
 
-playFace :: Texture -> Texture -> Renderer -> MyEvent -> IO ()
-playFace bkg face r e = case fmap eventPayload e of
-                             Right QuitEvent -> return ()
-                             Right (MouseMotionEvent mm) -> do copy r bkg Nothing Nothing
-                                                               copy r face Nothing . Just $ Rectangle (fromIntegral <$> mouseMotionEventPos mm) (V2 100 100)
-                             Left Redraw -> present r
-                             _ -> return ()
+playFace :: Texture -> Texture -> Renderer -> Sink MyEvent IO ()
+playFace bkg face r = do e <- await
+                         liftIO $ print e
+                         case fmap eventPayload <$> e of
+                              Nothing -> return ()
+                              Just (Right QuitEvent) -> return ()
+                              Just e' -> do go e'; playFace bkg face r
+                            
+
+    where go evt = case evt of
+                        Right (MouseMotionEvent mm) -> do copy r bkg Nothing Nothing
+                                                          copy r face Nothing . Just $ Rectangle (fromIntegral <$> mouseMotionEventPos mm) (V2 100 100)
+                                                          -- present r
+                        Left Redraw -> present r
+                        _ -> return ()
