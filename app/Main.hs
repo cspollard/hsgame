a5f803c3e1dfbfefbeb64fee8ddb4299f2d3dc0f
Chris Pollard <cspollard@gmail.com>
2016-08-19 10:12:09 +0100

    working from last night...

--- app/Main.hs
+++ app/Main.hs
@@ -6,17 +6,8 @@
 
 module Main where
 
-import Debug.Trace
-
 import Foreign.C.Types (CInt)
 
-import Data.Word (Word32)
-
-import Control.Concurrent.STM
-import Data.Conduit.TQueue
-import Data.Conduit.TMChan
-
-import Control.Monad (unless)
 import Control.Monad.IO.Class
 
 import Control.Lens
@@ -37,17 +28,6 @@ textureSize t = do ti <- queryTexture t
                    return $ V2 w h
 
 
-data Redraw = Redraw deriving Show
-type MyEvent = Either Redraw Event
-
-
-timer :: MonadIO m => Word32 -> Source m Redraw
-timer fps = do q <- liftIO . atomically $ newTBQueue 1
-               addTimer fps $ \_ -> do atomically $ writeTBQueue q Redraw
-                                       return (Reschedule fps)
-               sourceTBQueue q
-
-
 events :: MonadIO m => Source m Event
 events = do yield =<< waitEvent
             events
@@ -62,22 +42,25 @@ main = do initializeAll
           ts <- textureSize bkg
           windowSize window SDL.$= ts
 
-          evts <- runResourceT $ mapOutput Right events >=< mapOutput Left (timer 500)
-          evts $$ playFace bkg face renderer
+          events $$ playFace bkg face renderer
 
 
-playFace :: Texture -> Texture -> Renderer -> Sink MyEvent IO ()
-playFace bkg face r = do e <- await
-                         liftIO $ print e
-                         case fmap eventPayload <$> e of
-                              Nothing -> return ()
-                              Just (Right QuitEvent) -> return ()
-                              Just e' -> do go e'; playFace bkg face r
-                            
+playFace :: Texture -> Texture -> Renderer -> Sink Event IO ()
+playFace bkg face rend = do copy rend bkg Nothing Nothing
+                            copy rend face Nothing . Just $ Rectangle origin (V2 100 100)
+                            present rend
 
-    where go evt = case evt of
-                        Right (MouseMotionEvent mm) -> do copy r bkg Nothing Nothing
-                                                          copy r face Nothing . Just $ Rectangle (fromIntegral <$> mouseMotionEventPos mm) (V2 100 100)
-                                                          -- present r
-                        Left Redraw -> present r
-                        _ -> return ()
+                            go bkg face rend
+
+
+    where go b f r = do e <- await
+                        liftIO $ print e
+                        case e of
+                             Nothing -> go b f r
+                             Just evt -> case eventPayload evt of
+                                              QuitEvent -> return ()
+                                              MouseMotionEvent mm -> do copy r bkg Nothing Nothing
+                                                                        copy r face Nothing . Just $ Rectangle (fromIntegral <$> mouseMotionEventPos mm) (V2 100 100)
+                                                                        present r
+                                                                        go b f r
+                                              _ -> go b f r
